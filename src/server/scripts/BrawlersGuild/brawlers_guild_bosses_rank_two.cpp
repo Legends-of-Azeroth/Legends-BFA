816aeacfe209fde49cdaf578ce3a8a0c88dd0c75
Terragor <81566364+zTerragor@users.noreply.github.com>
2022-04-18 15:13:15 +0500

    Script/Brawlers : first part
    Script/Brawlers : first part

--- src/server/scripts/BrawlersGuild/brawlers_guild_bosses_rank_two.cpp
+++ src/server/scripts/BrawlersGuild/brawlers_guild_bosses_rank_two.cpp
@@ -1,6 +1,49 @@
+/*
+ * Copyright (C) 2022 Legends-of-Azeroth
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "AchievementMgr.h"
 #include "BrawlersGuild.h"
 
+enum eSpells
+{
+    // dippy
+    SPELL_PECK                  = 134537,
+
+    // bill
+    SPELL_SUMMON_BROOM          = 233158,
+    SPELL_SUMMON_BROOM_BOOM     = 236458,
+
+    // sanoriak
+    SPELL_FIREBALL              = 136333,
+    SPELL_FIREWALL              = 132666,
+    SPELL_PYROBLAST             = 136334,
+    SPELL_FLAME_BUFFET          = 135237,
+
+    // master
+    SPELL_DZEN                  = 229865,
+    SPELL_DZEN_SPHERE           = 229884,
+    SPELL_MAIN_STATE            = 229843,      
+};
+
+
 void AddSC_the_brawlers_guild_bosses_rank_two()
 {
+    //new boss_brawguild_dippy();
+    //new boss_brawguild_bill();
+    //new boss_brawguild_sanoriak();
+    //new boss_brawguild_master();
+    //new npc_brawguild_master_sphere();
 };
